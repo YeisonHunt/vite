05327314414f14218c328071db2be1487d038cb2
Yeison Eduardo Caicedo Garzon <caic800027@siigo.com>
2020-08-02 10:43:51 -0500

    PopJS Vite repo Vue

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hellooooo Vue 3.0 + Vite" />
+  <Another />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Another from './components/Another.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Another
+  }
+}
+</script>
---
+++ src/components/Another.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <span>Another component</span>
+        <div class="bg-gray-400">Another tailwind component</div>
+    </div>
+</template>
+
+<style lang="scss">
+    div {
+        span {
+            color: #ccc;
+            background-color: green;
+        }
+    }
+</style>
